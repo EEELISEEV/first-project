e5befc9c896e5a718bbb3162bfdb88b634362477
Gleb <Gleb@gmail.com>
2024-06-27 20:16:44 +0700

    добавлены классы в Praktikum и Converter

---
+++ Praktikum.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+class Praktikum {
+    public static void main(String[] args) {
+        HashMap<String, Double> Main = new HashMap<>();
+        Main.put("Петя", 350);
+        Main.put("Вася", 250);
+        System.out.println(Main);
+    }
+}
